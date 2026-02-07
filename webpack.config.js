de5bb059e659927c52f0217e1027082bf0a172e2
Victor Olasupo <volasupo5@gmail.com>
2026-02-07 10:37:39 +0100

    Update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -26,10 +26,6 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(woof2)/,
-        type: 'asset/resource',
       }
     ]
   }
