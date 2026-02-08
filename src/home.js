d54d6a381825e891b1de39a1790e39dae9d09e28
Victor Olasupo <volasupo5@gmail.com>
2026-02-08 10:07:34 +0100

    Dynamically create home tab contents
    A separate module was created to this effect

---
+++ src/home.js
@@ -0,0 +1,40 @@
+import img from '../images/chef.png';
+
+const doc = document;
+const mainContent = document.querySelector('#content');
+
+function createElementWithClass(tag, ...classes) {
+  const element = doc.createElement(tag);
+  if (classes.length > 0) {
+    element.classList.add(...classes)
+  }
+  return element
+}
+function addHomeTab() {
+  // create new elements to hold contents
+
+  const homeContainer = createElementWithClass('div', 'home');
+  const heroTextContainer = createElementWithClass('div');
+  const primaryText = createElementWithClass('p');
+  const secText = createElementWithClass('p');
+  const imgContainer = createElementWithClass('div', 'img-container');
+  const imgElement = createElementWithClass('img');
+  const ctaContainer = createElementWithClass('div');
+  const ctaText = createElementWithClass('p', 'cta');
+
+  primaryText.textContent = 'Best pizza in your country';
+  secText.textContent = 'Made with passion since 1950';
+  heroTextContainer.append(primaryText, secText);
+
+  imgElement.src = img;
+  imgElement.alt = 'A happy chef holding a mug.'
+  imgElement.style.width = '309px';
+  imgElement.style.height = '309px';
+  imgContainer.append(imgElement)
+
+  ctaText.textContent = 'Order online or visit us!';
+  ctaContainer.appendChild(ctaText);
+  
+  homeContainer.append(heroTextContainer, imgContainer, ctaContainer);
+  mainContent.appendChild(homeContainer);
+}
